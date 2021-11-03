d3f3f276a4cc61b2c52deed74f3ada25f6e6af81
Jason Kaufman <jkaufman217@gmail.com>
2021-11-03 02:14:31 -0700

    Added hooks for API calls to the server.
    Added proxy to client/package.json.
    Reverted changes to server/routes/index.js.
    Uninstalled cors package.

--- client/src/App.js
+++ client/src/App.js
@@ -1,47 +1,36 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      serverResponse: '',
-    };
-  }
+function App() {
+  const [data, setData] = useState(null);
 
-  callServer() {
-    fetch('http://localhost:3001/')
+  useEffect(() => {
+    fetch('/users')
       .then((res) => res.text())
-      .then((res) => this.setState({ serverResponse: res }))
-      .catch(err => err);
-  }
+      .then((text) => setData(text))
+      .catch((err) => console.log(err));
+  });
 
-  componentDidMount() {
-    this.callServer();
-  }
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-        <p>{this.state.serverResponse}</p>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+      <p>{!data ? "Loading..." : data}</p>
+    </div>
+  );
 }
 
 export default App;
