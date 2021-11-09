ddc0f4fb3ec5ae512d1285cf2177681ae5a5ddac
Jason Kaufman <jkaufman217@gmail.com>
2021-11-08 23:32:24 -0800

    Made Home component read from AboutMe.txt for my description.

--- client/src/Home.js
+++ client/src/Home.js
@@ -1,10 +1,20 @@
-import './Home.css'
+import { useState, useEffect } from 'react';
+import './Home.css';
 
 function Home() {
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    fetch('AboutMe.txt')
+      .then((res) => res.text())
+      .then((text) => setData(text))
+      .catch((err) => console.log(err));
+  });
+
   return (
     <div>
       <h1>Jason Kaufman</h1>
-      <h2 id='home-description'>Description Description Description Description Description Description Description Description Description Description Description Description</h2>
+      <h2 id='home-description'>{data}</h2>
     </div>
   );
 }
