9ce736f1b241e53b5fd84951b5a72540d6558dc2
Jason Kaufman <jkaufman217@gmail.com>
2022-03-10 20:50:40 -0800

    Properly made routing for each project. (#7)
    Moved the Projects.json fetch to App.
    Passed the Projects.json data as props to Projects.

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,9 @@
 import {
-  React,
+  React, 
+  useState, 
+  useEffect, 
+  lazy, 
+  Suspense,
 } from 'react';
 
 import {
@@ -11,10 +15,67 @@ import {
 import Home from './Home';
 
 function App () {
+  // State variable declarations.
+  const [data, setData] = useState(null);
+  const [paths, addToPaths] = useState([]);
+
+  // Get data from Projects.json on load.
+  useEffect(() => {
+    fetch('Projects.json')
+      .then((res) => res.json())
+      .then((_data) => setData(_data))
+      .catch((err) => console.log(err));
+  }, []);
+
+  // Fill the paths state variable after data has been successfully updated.
+  useEffect(() => {
+    if(data){
+      let listOfPaths = [];
+
+      for(let entry of data.projects)
+      {
+        listOfPaths.push(entry.route);
+      }
+
+      addToPaths(listOfPaths);
+    }
+  }, [data]);
+
+  // Creates a component using dynamic importing via React.lazy.
+  const createGenericComponent = (path) => {
+    const upperCasePath = path.charAt(0).toUpperCase() + path.slice(1);
+    const NewComponent = lazy(() => import('./external/' + upperCasePath + '/src/App'));
+
+    return <NewComponent/>;
+  }
+
+  // Creates a list of routes for the router based on the paths state variable.
+  const createNewRoutes = () => {
+    console.log('Projects loaded, creating routes');
+    
+    let components = [];
+    let id = 0;
+
+    for(let path of paths)
+    {
+      components.push(
+        <Route path={'/' + path} 
+               key={id++} 
+               element={<Suspense fallback={<>...</>}>
+                          {createGenericComponent(path)}
+                        </Suspense>}
+        />
+      );
+    }
+
+    return components;
+  }
+
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<Home/>}/>
+        <Route exact path='/' element={<Home projectsData={data}/>}/>
+        {!data ? console.log('Projects not loaded, no additional routes created') : createNewRoutes()}
       </Routes>
     </Router>
   );
--- client/src/Home.js
+++ client/src/Home.js
@@ -11,10 +11,9 @@ function Home(props) {
       </header>
       <About/>
       <WorkHistory/>
-      <Projects/>
+      <Projects projectsData={props.projectsData}/>
     </div>
   );
 }
 
 export default Home;
-// Rename to Home, make a new file for App. App will determine which page we are looking at (home or a single project) via props to the Projects component.
