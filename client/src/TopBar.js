bba24d5d8ede9db7f1610e8f1b599c721a58133f
Jason Kaufman <jkaufman217@gmail.com>
2022-03-23 00:27:57 -0700

    Fixed up the style of the page. (#10)
    Picked out better colors.
    Removed test data from Projects.json.

--- client/src/TopBar.js
+++ client/src/TopBar.js
@@ -5,18 +5,16 @@ import { MdOutlineEmail } from 'react-icons/md';
 
 function TopBar() {
   return (
-    <div>
-      <header className="Home-header">
-        <div className='contact-info'>
-          <a className="email" href={"mailto:jkaufman217@gmail.com"}>
-            <MdOutlineEmail alt='jkaufman217@gmail.com' size={35}/>
-          </a>
-          <a className="linkedin" href={"https://www.linkedin.com/in/jason-m-kaufman/"} target='_blank' rel='noreferrer'>
-            <BsLinkedin alt='Linkedin' size={30}/>
-          </a>
-        </div>
-      </header>
-    </div>
+    <header className="Home-header">
+      <div className='contact-info'>
+        <a className="email" href={"mailto:jkaufman217@gmail.com"} target='_blank' rel='noreferrer'>
+          <MdOutlineEmail alt='jkaufman217@gmail.com' size={35}/>
+        </a>
+        <a className="linkedin" href={"https://www.linkedin.com/in/jason-m-kaufman/"} target='_blank' rel='noreferrer'>
+          <BsLinkedin alt='Linkedin' size={30}/>
+        </a>
+      </div>
+    </header>
   );
 }
 
