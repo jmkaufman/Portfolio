abaad1c84e54e1d2aed56014857f52a051212576
Jason Kaufman <jkaufman217@gmail.com>
2022-03-23 20:54:19 -0700

    Added thumbnails for the projects.
    Included a thumbnail for Pong.
    Adjusted the project block border.
    Added border to thumbnails.
    Adjusted content width.

--- client/src/ProjectsBlock.js
+++ client/src/ProjectsBlock.js
@@ -7,6 +7,8 @@ function ProjectsBlock(props){
     <div className='project-block'>
       <h3>{props.entry.title}</h3>
       <p>{props.entry.description}</p>
+      <img className="thumbnail" src={require('../public/thumbnails/' + props.entry.thumbnail).default} alt={props.entry.title + ' thumbnail'}/> {/*Try to keep thumbnails 350x233*/}
+      <br/>
       <Link to={'/' + props.entry.route} target='_blank' rel='noreferrer'>View it here!</Link>
       <br/>
       <a href={props.entry.repo} target='_blank' rel='noreferrer'>Github Repository</a>
